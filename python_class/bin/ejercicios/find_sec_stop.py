93ce5cb49999ae695c34515d45096ee8ac097aa1
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:32:00 -0600

    add secuencias de paro

--- python_class/bin/ejercicios/find_sec_stop.py
+++ python_class/bin/ejercicios/find_sec_stop.py
@@ -1,5 +1,6 @@
+#pedimos al usuario ingresar secuencias de DNA
 secuencias = input ("Dame secuencias separadas por comas").split(",")
-
+#indicamos cuáles son las secuencias de paro que hay que reconocer
 secuencias_stop = [ secuencia for secuencia in secuencias if "TAA" in secuencia or "TAG" in secuencia or "TGA" in secuencia]
 
 print (secuencias_stop)
