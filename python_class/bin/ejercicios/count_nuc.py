10722f8c0e16512c984eadd8e1748348f005b018
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:09:08 -0600

    count_nuc.py

--- python_class/bin/ejercicios/count_nuc.py
+++ python_class/bin/ejercicios/count_nuc.py
@@ -1,3 +1,5 @@
+#Pide al usuario que ingrese varias secuencias de ADN separadas por comas
 secuencias = input ("Dame secuencias separadas por comas").upper().split(",")
+#usamos el método count para contar cada letra en cada secuencia
 conteo = [(f"A: {secuencia.count('A')}", f"T: {secuencia.count('T')}", f"G: {secuencia.count('G')}", f"C: {secuencia.count('C')}") for secuencia in secuencias]
 print(conteo)
