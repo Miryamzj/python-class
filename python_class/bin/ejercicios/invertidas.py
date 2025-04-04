09584381dcd1302c162544732f17bf9c70f3709b
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:28:52 -0600

    add inverttidas.py

--- python_class/bin/ejercicios/invertidas.py
+++ python_class/bin/ejercicios/invertidas.py
@@ -1,5 +1,6 @@
+#Pedimos al usuario ingresar secuencias para invertirlas después 
 secuencias = input ("Dame secuencias separadas por comas").split(",")
-
+#invertimos la secuencia con .strip()[::-1[]
 secuencias_invertidas=[secuencia.strip()[::-1] for secuencia in secuencias]
 
 print (secuencias_invertidas)
