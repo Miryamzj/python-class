79b0b10146f93f23bcafed24f8cf29a3a736194c
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:34:34 -0600

    add tsv to fasta.py

--- python_class/bin/ejercicios/tsv-to-fasta.py
+++ python_class/bin/ejercicios/tsv-to-fasta.py
@@ -1,7 +1,8 @@
+#le damos un archivo de entrada como un input
 inputfile = "dna_sequences.txt"
 outputfile = "dna_sequences.fasta"
 
-#Lee el archivo de entrada dna_sequences.txt
+#Lee el archivo de entrada dna_sequences.txt y generamos un archivo de salida 
 with open (inputfile,"r") as infile, open(outputfile,"w") as outfile:
     for linea in infile: 
         id, seq = linea.strip().split("\t")
