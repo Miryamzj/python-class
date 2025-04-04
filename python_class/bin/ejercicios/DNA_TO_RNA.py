1f62c64239418fed59f8123766d8f88896127102
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:23:34 -0600

    add to dna.to rna

--- python_class/bin/ejercicios/DNA_TO_RNA.py
+++ python_class/bin/ejercicios/DNA_TO_RNA.py
@@ -1,5 +1,6 @@
+#Pedimos al usuario ingresar secuencias 
 secuencias = input ("Dame secuencias separadas por comas").split(",")
-
+#Para reemplazar las T por U 
 secuencias_arn =[secuencia.replace("T", "U") for secuencia in secuencias]
 
 print (secuencias_arn)
