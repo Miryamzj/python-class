01a8ebcc17af8d38e9cba7f516ee6a6d5623aa23
Miryamzj <miryamzj@lcg.unam.mx>
2025-04-03 19:20:49 -0600

    add cut3.py

--- python_class/bin/ejercicios/cut3.py
+++ python_class/bin/ejercicios/cut3.py
@@ -1,5 +1,6 @@
+#pedimos al usuario ingresar secuencias separadas 
 secuencias = input ("Dame secuencias separadas por comas").split (",")
-    
+#las primeras 3 letras serán los codones de incio     
 codones_de_inicio = [secuencia.strip()[:3] for secuencia in secuencias]
 
 print (codones_de_inicio)
