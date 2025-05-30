a0292a129275c0cd653f725858ce20e243e034b4
Miryamzj <miryamzj@lcg.unam.mx>
2025-05-30 11:25:31 -0600

    Añadiendo argparse a main

--- peak_analysis-main/Extractor_secuencias/bin/main.py
+++ peak_analysis-main/Extractor_secuencias/bin/main.py
@@ -1,19 +1,23 @@
 import os
-#AVANCE 2 PROYECTO FINAL 
-#Usamos las funciones de los módulos previamente hechos
+import argparse
+
+# Importamos funciones definidas en otros módulos
 from genome import cargar_genoma
 from peaks import leer_archivo_picos, extraer_secuencias
 from io_utils import guardar_fasta_por_tf
 
-def main():
-    # Rutas relativas desde la carpeta del programa 
-    # Con base_dir y os, ya se construyen rutas relativas al programa, sin importar desde qué carpeta lo ejecute
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(base_dir, "data")
-    output_dir = os.path.join(base_dir, "output")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Extrae secuencias FASTA para factores de transcripción en E. coli")
+    parser.add_argument("--genoma", required=True, help="Ruta al archivo FASTA del genoma")
+    parser.add_argument("--picos", required=True, help="Ruta al archivo TSV con coordenadas de picos")
+    parser.add_argument("--salida", required=True, help="Directorio de salida para archivos FASTA")
+    return parser.parse_args()
 
-    genome_path = os.path.join(data_dir, "ecoli_genome.fasta")
-    peaks_path = os.path.join(data_dir, "union_peaks_file.tsv")
+def main():
+    args = parse_args()
+    genome_path = args.genoma
+    peaks_path = args.picos
+    output_dir = args.salida
 
     try:
         genoma = cargar_genoma(genome_path)
